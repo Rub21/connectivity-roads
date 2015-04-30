a579d61b85fe7f1152b9821e8991c8ee02be5840
Rub21 <ruben@mapbox.com>
2015-04-29 23:27:42 -0500

    trying to async

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('cities.geojson', 'utf8'));
-var OSRM = require('osrm-client');
+
 var _ = require('underscore');
-var osrm = new OSRM("http://router.project-osrm.org/");
+//var OSRM = require('osrm-client');
+//var osrm = new OSRM("http://router.project-osrm.org/");
+
+var OSRM = require('osrm');
+var osrm = new OSRM("nepal.osrm");
+
+var async = require("async");
 
 function SortByName(x, y) {
 	return ((x.properties.name == y.properties.name) ? 0 : ((x.properties.name > y.properties.name) ? 1 : -1));
@@ -10,6 +16,8 @@ function SortByName(x, y) {
 obj.features = obj.features.sort(SortByName);
 var start = _.map(obj, _.clone);
 var end = _.map(obj, _.clone);
+
+
 
 _.each(start[2], function(start) {
 	var coor_start = _.map(start.geometry.coordinates, _.clone);
@@ -22,7 +30,7 @@ _.each(start[2], function(start) {
 
 		if (start.properties.name != end.properties.name) {
 			var url = "http://map.project-osrm.org/?hl=de&loc=" + coor_start + "&loc=" + coor_end;
-			console.log(start.properties.name + "-->:" + end.properties.name + " | " + url);
+			//console.log(start.properties.name + "-->:" + end.properties.name + " | " + url);
 
 			var query = {
 				coordinates: [
@@ -35,10 +43,65 @@ _.each(start[2], function(start) {
 					console.log(result.route_summary);
 				} else {
 
-					console.log('test');
+					console.log('Fail');
 				}
 
 			});
 		}
 	});
 });
+
+
+// async.each(start[2], function(start, callback) {
+// 	var coor_start = _.map(start.geometry.coordinates, _.clone);
+// 	coor_start = coor_start.reverse();
+
+// 	async.each(end[2], function(end, callback) {
+
+// 		var coor_end = _.map(end.geometry.coordinates, _.clone);
+// 		coor_end = coor_end.reverse();
+
+// 		if (start.properties.name != end.properties.name) {
+// 			var url = "http://map.project-osrm.org/?hl=de&loc=" + coor_start + "&loc=" + coor_end;
+// 			//console.log(start.properties.name + "-->:" + end.properties.name + " | " + url);
+
+// 			var query = {
+// 				coordinates: [
+// 					coor_start,
+// 					coor_end
+// 				]
+// 			};
+// 			osrm.route(query, function(err, result, callbackroutes) {
+// 				if (result.route_summary !== undefined) {
+// 					console.log(result.route_summary);
+// 					callbackroutes;
+// 				} else {
+
+// 					console.log('test');
+// 				}
+
+
+
+// 			});
+
+
+// 		}
+
+
+// 		callback();
+// 	}, function(err) {
+// 		console.log('--');
+
+// 	});
+// 	callback();
+
+// }, function(err) {
+// 	console.log('+++');
+
+// });
+
+
+
+// function callbackroutes() {
+// 	console.log('+=');
+// }
